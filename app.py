deb1e01586967a2d99dee13bed942f6ec19edd2d
Sri Sai Abhishake Gopal Dasari <srisaiabhishakegopal@gmail.com>
2020-12-16 09:27:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+# initialize our Flask application
+app= Flask(__name__)
+
+@app.route("/hello", methods=["GET"])
+
+
+def hello():
+    return "Hello World!"
+
+@app.route("/class/name", methods=["GET"])
+def name():
+    return "This is CS446! This is Dasari's server!"
+
+#  main thread of execution to start the server
+if __name__=='__main__':
+    app.run( port="8888")
